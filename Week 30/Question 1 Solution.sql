97e9d675f9f50ce0b4b73b5a71a84a40a8efec87
Abasifreke Nkanang <nkanangabasifreke.data@gmail.com>
2023-03-09 13:10:38 +0100

    Added Solution query to Question 1

---
+++ Week 30/Question 1 Solution.sql
@@ -0,0 +1,6 @@
+SELECT EXTRACT(MONTH FROM submit_date) AS mth,
+        product_id AS product,
+        ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY mth, product
+ORDER BY mth, product;
